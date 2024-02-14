3622cd35273d639bae4add0f9c4ca141bedcf8fe
ValDesign <valdesign.dev@gmail.com>
2024-02-14 14:21:31 +0100

    Add default config file if it doesn't exist

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
 import yaml
 
 class Config:
   def __init__(self):
+    if not os.path.exists("assets/config.yaml"):
+      with open("assets/config.yaml", "w") as file:
+        file.write("config:\n  resolution: 256x256\n  frame_rate: 30\n  render_distance: 10\n  max_walls: 10\n  fullscreen: false\ncolors:\n  _: 7\n  #: 8\nassets:\n  map: assets/map.txt\n")
+    
     with open("assets/config.yaml", "r") as file:
       self.data = yaml.safe_load(file)
   
